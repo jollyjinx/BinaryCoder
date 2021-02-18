bf92734bbbe0e4801df8367770f5fd43c0a7b42a
Patrick Stein <Patrick.Stein@jinx.de>
2021-02-18 12:23:57 +0100

    added data reading

--- Source/BinaryDecoder.swift
+++ Source/BinaryDecoder.swift
@@ -84,7 +84,7 @@ public extension BinaryDecoder {
 
     func decode(_ type: Data.Type, length: Int) throws -> Data
     {
-        var data:Data
+        let data:Data
 
         try self.read(length,into: &data)
         return data
